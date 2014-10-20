2b2ac8f408b0e5efc48c5ceb949c8ccc2aceb5be
XaveScor <XaveScor@gmail.com>
2014-10-20 19:41:53 +0600

    Create main.c
    part 1

---
+++ course2/sem1/main.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#include "iolist.h"
+
+void clearList(pNode *list) {
+	pNode next;
+	while (*list) {
+		next = (*list)->next;
+		free((*list)->value);
+		free(*list);
+		(*list) = next;
+	}
+}
+
+void pushList(char *str, pNode *list) {
+    if (!strlen(str))
+        return;
+
+	pNode el = (pNode)malloc(sizeof(Node));
+	assert(el);
+	el->value = (char *)malloc(sizeof(char) * strlen(str));
+	assert(el->value);
+
+	strcpy(el->value, str);
+
+	el->next = (*list);
+	(*list) = el;
+}
+
+void addSymbol(char ch, char **str, size_t *cur, size_t *len) {
+    if (*cur + 1 >= *len) {
+        *len = (size_t)(MULTIPLYER * (*len) + 1.0);
+        *str = (char *)realloc(*str, sizeof(char) * (*len));
+    }
+    assert(*str);
+    *(*str + (*cur)++) = ch;
+}
+
+void printList(pNode list) {
+    if (list == NULL)
+        return;
+
+    printList(list->next);
+    printf("(%s) ", list->value);
+}
+
+char input(pNode *list) {
+	size_t cur = 0,
+           len = 1;
+	char ch = '\0', *str = NULL;
+	bool flag = false, state = false;;
+
+	clearList(list);
+	clearStr(&str);
+
+	while (ch = getchar()) {
+        if (state)
+            switch(ch) {
+                case EOF:
+                case DELIMITER:
+                case BORDER:
+                    state = false;
+                    if (ch == BORDER)
+                        ch = SEPARATOR;
+                    break;
+                default:
+                    addSymbol(ch, &str, &cur, &len);
+                    continue;
+            }
+
+		switch (ch) {
+			case BORDER:
+			    state = true;
+				break;
+
+			default:
+				addSymbol(ch, &str, &cur, &len);
+				break;
+
+            case DELIMITER:
+			case EOF:
+			    flag = true;
+			case SEPARATOR:
+				str[cur] = '\0';
+				pushList(str, list);
+
+				clearStr(&str);
+				cur = 0;
+				len = 1;
+
+				if (flag) return ch;
+		}
+	}
+}
+
+void clearStr(char **str) {
+    if (*str)
+        free(*str);
+
+    *str = (char *)malloc(sizeof(char));
+    assert(*str);
+    **str = '\0';
+}
+
+int main() {
+    pNode list = NULL;
+    char ch;
+
+	while (ch = input(&list)) {
+        printList(list);
+        putchar('\n');
+        if (ch == EOF)
+            break;
+	}
+
+    #if defined(_WIN32) && !DEBUG
+        system("pause");
+    #endif
+	return 0;
+}
